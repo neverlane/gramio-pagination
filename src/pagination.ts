19c48ee8c573ab09867d8b1f5d68f7bcd176d0ff
kravetsone <57632712+kravetsone@users.noreply.github.com>
2025-05-30 23:56:59 +0300

    feat: more work + add callback-data

--- src/pagination.ts
+++ src/pagination.ts
@@ -1,13 +1,21 @@
+import { CallbackData } from "@gramio/callback-data";
 import { InlineKeyboard } from "@gramio/keyboards";
 import type { PaginationDataFunction } from "./types.ts";
 
 export class Pagination<Data> {
+	private name: string;
 	private getData: PaginationDataFunction<Data>;
 	private limitValue = 10;
 	private columnsValue: number | undefined;
 
-	constructor(func: PaginationDataFunction<Data>) {
+	private callbackData: CallbackData;
+
+	constructor(name: string, func: PaginationDataFunction<Data>) {
+		this.name = name;
 		this.getData = func;
+		this.callbackData = new CallbackData(name)
+			.enum("type", ["set", "select"])
+			.number("offset");
 	}
 
 	limit(count: number) {
@@ -25,6 +33,38 @@ export class Pagination<Data> {
 	async getKeyboard(offset: number) {
 		const data = await this.getData({ offset, limit: this.limitValue + 1 });
 
-		return new InlineKeyboard().columns(this.columnsValue);
+		return new InlineKeyboard({
+			enableSetterKeyboardHelpers: true,
+		})
+			.columns(this.columnsValue)
+			.add(
+				...data.map((x) =>
+					InlineKeyboard.text(
+						x.title,
+						this.callbackData.pack({ type: "select", offset: x.id }),
+					),
+				),
+			)
+			.row()
+			.addIf(
+				offset !== 0,
+				InlineKeyboard.text(
+					"⬅️",
+					this.callbackData.pack({
+						type: "set",
+						offset: offset - this.limitValue,
+					}),
+				),
+			)
+			.addIf(
+				data.length > this.limitValue,
+				InlineKeyboard.text(
+					"➡️",
+					this.callbackData.pack({
+						type: "set",
+						offset: offset + this.limitValue,
+					}),
+				),
+			);
 	}
 }
